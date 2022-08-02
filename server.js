9a9b05e85c1db53fc3a6b18b44f2ee0f8bbf7f80
juanmgiron <juan.giron@teladoc.com>
2022-08-02 15:48:33 -0300

    new creation panel, no db

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const OpenTok = require("opentok")
-const mysql = require('mysql')
 require('dotenv').config()
 
 const apiKey = process.env.API_KEY
@@ -32,18 +31,6 @@ app.use(function (req, res, next) {
 });
 app.use(express.json())
 
-var db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: ''
-})
-
-db.connect(function(err) {
-    if (err) throw err
-    console.log('db connected')
-})
-
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -51,19 +38,8 @@ app.get('/', (req, res) => {
 app.post('/createSession', (req, res) => {
     opentok.createSession(function (err, session) {
         if (err) return console.log(err);
-        db.query('INSERT INTO session (sessionId, name) VALUES ("'+session.sessionId+'", "'+req.body.name+'")', (error, results, fields) => {
-            if (error) throw error;
-            res.send(session.sessionId)
-        })
+        res.send(session.sessionId)
     });
-})
-
-app.get('/sessions', (req, res) => {
-    db.query('SELECT * FROM session', (error, results, fields) => {
-        if (error) throw error;
-        results = JSON.parse(JSON.stringify(results));
-        res.send(results)
-    })
 })
 
 app.post('/token', (req, res) => {
--- src/components/CreationPanel.js
+++ src/components/CreationPanel.js
@@ -1,59 +1,32 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import {Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function CreationPanel() {
-    const [name, setName] = useState('');
-    const [sessions, setSessions] = useState([]);
-
-    useEffect(() => {
-        axios.get('/sessions')
-            .then((results) => {
-                setSessions(results.data)
-            })
-    },[])
-
-    const showSessions = sessions.map(
-        (session) => 
-        <li key={session.sessionId} className="list-group-item">
-            <div className="row">
-                <div className="col-sm" align="center">
-                    {session.name}
-                </div>
-                <div className="col-sm" align="center">
-                    <Link to={"/publisher"} state={{ id: session.sessionId }}>
-                        <button className="btn btn-primary">publish</button>
-                    </Link>
-                </div>
-                <div className="col-sm" align="center">
-                    <Link to={"/subscriber"} state={{ id: session.sessionId }}>
-                        <button className="btn btn-primary">subscribe</button>
-                    </Link>
-                </div>
-            </div>
-        </li>
-    )
+    const [sessionId, setSessionId] = useState();
 
     const handleCreation = () => {
-        axios.post('/createSession', {
-            name: name
-        }).then((result) => {
-            var session = {name: name, sessionId: result.data}
-            setSessions([...sessions, session])
+        axios.post('/createSession').then((result) => {
+            setSessionId(result.data)
         })
     }
 
     return (
         <div className="container">
             <div className="input-group mb-3 my-5">
-                <input value={name} onChange={(e) => setName(e.target.value)} type='text' className="form-control"/>
                 <div className="input-group-append">
-                    <button onClick={handleCreation} className="btn btn-outline-secondary">create session</button>
+                    <button onClick={handleCreation} className="btn btn-primary">create session</button>
+                </div>
+                <input value={sessionId} onChange={e => setSessionId(e.target.value)} type='text' className="form-control"/>
+                <div className="input-group-append">
+                    <Link to={"/publisher"} state={{ id: sessionId }}>
+                        <button className="btn btn-outline-secondary" disabled={!sessionId}>publish to session</button>
+                    </Link>
+                    <Link to={"/subscriber"} state={{ id: sessionId }}>
+                        <button className="btn btn-outline-secondary" disabled={!sessionId}>subscribe to session</button>
+                    </Link>
                 </div>
             </div>
-            <ul className="list-group">
-                {showSessions}
-            </ul>
         </div>
     )
 }
